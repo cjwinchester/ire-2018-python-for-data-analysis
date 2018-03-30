6adf9c69ed57afd714161ff4f8d1a3070b04da0e
Cody Winchester <cjwinchester@gmail.com>
2018-03-30 16:24:47 -0600

    raw link

--- appendix/myfile.py
+++ appendix/myfile.py
@@ -2,5 +2,5 @@ codys_dog = {
     'name': 'Charlie',
     'breed': '?',
     'age_mos': 12,
-    'picture': 'https://github.com/cjwinchester/ire-2018-python-for-data-analysis/blob/master/img/charlie.jpg'
+    'picture': 'https://github.com/cjwinchester/ire-2018-python-for-data-analysis/raw/master/img/charlie.jpg'
 }
